592a06ce9f63dca9622851e38e0d260d94f9c400
khizer713 <ken.hizer@gmail.com>
2025-06-25 08:35:10 -0400

    fixed bug with random text

--- streamlit-app/app.py
+++ streamlit-app/app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.title("Mortgage Document Classifier")
-st.write("Hello, Streamlit!") git remote add origin https://github.com/YOUR_USERNAME/mortgage_streamlit_app.git
+st.write("Hello, Streamlit!") 
